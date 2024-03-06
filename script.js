02774e5ba3745449ec20e5c2fa2241be23a6844b
Elhan23 <elhan13chik@gmail.com>
2024-03-05 19:53:53 -0800

    first init

---
+++ script.js
@@ -0,0 +1,12 @@
+// const URL = "https://jsonplaceholder.typicode.com"
+ 
+
+// const getAsyncData = async () => {
+//   try {
+//     const response = await fetch(URL);
+//     const data = await response.json();
+//     console.log(data);
+//   } catch {
+
+//   }
+// }
